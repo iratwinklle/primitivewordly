7e0e557061b8bde1adff184d1bf73e1bd9211e1a
iratwinklle <iratwinkle@gmail.com>
2022-02-02 17:14:25 +0300

    check word for letters in today_word with "for" loop

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 # str for word
-#word = input("Введите слово из 5 букв ")
-word = 'шлкуд'
+# word = input("Введите слово из 5 букв ")
+word = 'ккооо'
 today_word = 'кошка'
-#result
+# result
 if word == today_word:
     print(word + " - верное")
 else:
-    # if word[0] in today_word:
-    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[0])
-    # if word[1] in today_word:
-    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[1])
-    # if word[2] in today_word:
-    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[2])
-    # if word[3] in today_word:
-    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[3])
-    # if word[4] in today_word:
-    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[4])
-
-    ### ####
-for word in [0, 1, 2, 3, 4]:
-    print("Слово не угадано! Но в загаданном слове есть буква:" + word[0, 1, 2, 3, 4])
+    print("Слово не угадано! Но в загаданном слове есть буквы:")
+    for i in word:
+        if i in today_word:
+            print(i)
