61e876eaa12b219e5e48ea0efdf17525cc6418fa
iratwinklle <iratwinkle@gmail.com>
2022-02-02 15:38:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# str for word
+#word = input("Введите слово из 5 букв ")
+word = 'шлкуд'
+today_word = 'кошка'
+#result
+if word == today_word:
+    print(word + " - верное")
+else:
+    # if word[0] in today_word:
+    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[0])
+    # if word[1] in today_word:
+    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[1])
+    # if word[2] in today_word:
+    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[2])
+    # if word[3] in today_word:
+    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[3])
+    # if word[4] in today_word:
+    #     print("Слово не угадано! Но в загаданном слове есть буква:" + word[4])
+for word in [0, 1, 2, 3, 4]:
+    print("Слово не угадано! Но в загаданном слове есть буква:" + word[0, 1, 2, 3, 4])
